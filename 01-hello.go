c0d8db4fa142230e88e73e69926f09a2a36e933e
Harry Nguyen <harrynguyen@Harrys-MacBook-Pro.local>
2019-01-25 20:02:58 +1100

    Initial commit

---
+++ 01-hello.go
@@ -0,0 +1,13 @@
+package main  
+
+// Import Library
+import (
+	"fmt"
+	"time"
+)
+
+// Main function, the program will run this function frist 
+func main () {
+	fmt.Println("Hello 🤪")
+	fmt.Println("The time is", time.Now())
+}

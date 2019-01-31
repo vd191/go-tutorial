db276bec414815e056a4a5ab80b270f91691aa2d
Harry Nguyen <harrynguyen@Harrys-MacBook-Pro.local>
2019-01-31 19:24:34 +1100

    Hello World

--- 01-hello.go
+++ 01-hello.go
@@ -1,4 +1,4 @@
-package main  
+package main
 
 // Import Library
 import (
@@ -6,8 +6,15 @@ import (
 	"time"
 )
 
-// Main function, the program will run this function frist 
-func main () {
+// Main function, the program will run this function frist
+func main() {
 	fmt.Println("Hello 🤪")
 	fmt.Println("The time is", time.Now())
 }
+
+/*
+	CONCLUSION: 1 Trương trình viết bằng golang sẽ có 3 phần:
+	package
+	import
+	function main.
+*/

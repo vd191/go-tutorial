33c92d0a939b63f709a576478bfc9d7b46ad866c
Harry Nguyen <harrynguyen@Harrys-MacBook-Pro.local>
2019-01-31 19:25:00 +1100

    Variable

--- 02-variable.go
+++ 02-variable.go
@@ -1,7 +1,10 @@
-package main 
+package main
 
-import "fmt"
-import "math/cmplx"
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+)
 
 // The var statement declares a list of variables
 var batman, sup, flash bool
@@ -11,9 +14,9 @@ var power, strength int = 100, 400
 
 // Basic types
 var (
-	ToBe bool = false
-	MaxInt uint64 = 1<<64 - 1
-	z complex128 = cmplx.Sqrt(-5 + 12i)
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 func main() {
@@ -23,15 +26,14 @@ func main() {
 	// The variable will take the type of the initializer.
 	var fly, invisible = true, "no"
 
-	// := statement can be used intead of var keyword. 
+	// := statement can be used intead of var keyword.
 	joker := false
-	fmt.Println(power, strength, fly, invisible, joker )
+	fmt.Println(power, strength, fly, invisible, joker)
 
 	// Test basic type
 	fmt.Printf("Type: %T Value: %v \n", ToBe, ToBe)
 	fmt.Printf("Type: %T Value: %v \n", MaxInt, MaxInt)
 	fmt.Printf("Type: %T Value: %v \n", z, z)
-
 
 	// Type conversions
 	var x, y int = 3, 4
@@ -40,26 +42,14 @@ func main() {
 	fmt.Println(x, y, z)
 
 	// Constants cannot be declared using the := syntax.
-	const sun = "☀️";
+	const sun = "☀️"
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*
+	CONCLUSION:
+	Biến trong golang được khai báo bằng từ khoá var.
+	Hoặc viết tắt := để bỏ từ khoá var và khai báo data type
+	Có thể khai báo nhiều biến cùng data type cùng lúc.
+	Có thể chuyển đổi data type bằng cách khai báo data type lại
+*/

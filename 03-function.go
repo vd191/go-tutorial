e4ad9fb2382e0ddec5b53712751b1ba9c2aae4e9
Harry Nguyen <harrynguyen@Harrys-MacBook-Pro.local>
2019-01-31 19:25:13 +1100

    Function

--- 03-function.go
+++ 03-function.go
@@ -1,21 +1,22 @@
 package main
 
 import "fmt"
-// Or can also write multiple import statement. 
+
+// Or can also write multiple import statement.
 import "math"
 
-func main () {
+func main() {
 	// Print pi number
 	// a name is exported if it begins with a capital letter.
 	fmt.Println(math.Pi)
 
-	// Call add function 
-	fmt.Println(add(1,1))
+	// Call add function
+	fmt.Println(add(1, 1))
 
-	// Retrieve 2 returns form function. 
-	a,b := swap("😝", "🧐")
-	//Call swap function 
-	fmt.Println(a,b)
+	// Retrieve 2 returns form function.
+	a, b := swap("😝", "🧐")
+	//Call swap function
+	fmt.Println(a, b)
 
 	// Named return values
 	fmt.Println(split(20))
@@ -28,18 +29,25 @@ func add(x int, y int) int { // (x , y int) : Functions continued
 }
 
 // Function return multiple results.
-func swap(a, b string) ( string, string ) {
+func swap(a, b string) (string, string) {
 	return b, a
 }
 
-// Go's return values may be named. 
+// Go's return values may be named.
 // If so, they are treated as variables defined at the top of the function.
 func split(sum int) (x, y int) {
 	x = sum * 4 / 9
 	y = sum - x
-	
-	//A return statement without arguments returns the named return values. 
-	// This is known as a "naked" return.
-	return 
-} 
 
+	//A return statement without arguments returns the named return values.
+	// This is known as a "naked" return.
+	return
+}
+
+/*
+	CONCLUSION:
+	function signature:
+	func [function name] ( [argument] ) [return type] {}
+
+	Function có thể return nhiều giá trị.
+*/

b1e9ce857d2d5443b7d704d136cc278f090d40f3
Harry Nguyen <harrynguyen@Harrys-MacBook-Pro.local>
2019-01-31 19:26:22 +1100

    Pointer

--- 05-pointer.go
+++ 05-pointer.go
@@ -1,27 +1,24 @@
-package main 
+package main
 
 import "fmt"
 
 // Go has pointers. A pointer holds the memory address of a value.
-func main () {
-	
-
+func main() {
 
 	// Declare a variable x = 10
 	x := 10
-	// Call changeValue() function for changing value of x to 0 
+	// Call changeValue() function for changing value of x to 0
 	changeValue(x)
-	// Print out x variable 
-	fmt.Println(x) // x = 10 
-	// -> x = 10 because the value of variable x is referenced to a different 
-	// x variable of changeValue() function. that why that function didn't affect to 
-	// the real x variable. 
+	// Print out x variable
+	fmt.Println(x) // x = 10
+	// -> x = 10 because the value of variable x is referenced to a different
+	// x variable of changeValue() function. that why that function didn't affect to
+	// the real x variable.
 
 	// --------------------------------------------------------------------
-	y := 20 
+	y := 20
 	changeValueWithPointer(&y)
 	fmt.Println(y)
-
 
 	// --------------------------------------------------------------------
 	z := new(int)
@@ -30,7 +27,7 @@ func main () {
 
 	// --------------------------------------------------------------------
 	a := 1
-	// & to get the memory adrress of a. 
+	// & to get the memory adrress of a.
 	b := &a
 	fmt.Println(b) // 0xc00006e018
 
@@ -39,24 +36,23 @@ func main () {
 
 }
 
-
-func changeValue (x int) {
+func changeValue(x int) {
 	x = 0
 }
 
-
-func changeValueWithPointer (y *int) {
+func changeValueWithPointer(y *int) {
 	*y = 0
 }
 
-
-func changeValueWithNewKeyword (z * int) {
+func changeValueWithNewKeyword(z *int) {
 	*z = 0
 }
 
-
-
-
-
-
-
+/*
+	CONCLUSION:
+	khi truyền 1 biến vào 1 hàm thì trương trình sẽ tạo 1 biến reference của biến ấy
+	chứ không trực tiếp sử dụng biến. Bởi vậy, khi thực hiện thay đổi giá trị thì biến
+	không thay đổi. Đó là lý do cần sử dụng Pointer.
+	* : lấy giá trị của biến
+	& : lấy địa chỉ memory của biến
+*/
